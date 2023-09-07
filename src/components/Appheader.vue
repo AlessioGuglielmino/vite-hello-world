ae024948797c06cf69de3b228889e815e9ec7f25
AlessioGugielmino <guglielmino.alessio@virgilio.it>
2023-09-07 17:40:32 +0200

    fix-folder

---
+++ src/components/Appheader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+    return {
+      links: ["link 1 ", "link 2 ", "link 3 "],
+    };
+  },
+};
+</script>
+
+<template>
+  <ul>
+    <li v-for="link in links">
+      {{ link }}
+    </li>
+  </ul>
+</template>
+
+<style>
+li {
+  color: white;
+  display: inline-block;
+}
+</style>
