6df0f53a66688235aa4e5315dac7ec96b57d50d2
AlessioGugielmino <guglielmino.alessio@virgilio.it>
2023-09-07 17:30:52 +0200

    esercizio-completo

---
+++ vite-project/src/App.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "La mia prima app con Vite!",
+    };
+  },
+};
+</script>
+
+<template>
+  <main>
+    <h1>{{ title }}</h1>
+  </main>
+</template>
+
+<style>
+main {
+  background-color: black;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+h1 {
+  text-align: center;
+  font-size: 3rem;
+  font-family: Arial, Helvetica, sans-serif;
+  color: blueviolet;
+}
+</style>
